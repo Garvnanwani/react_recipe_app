ebb89a08eb17bd69ea41d22c1a5103197c513859
Garv nanwani <garvnanwani16@gmail.com>
2020-11-01 23:23:24 +0530

    added edit funcitionality

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -1,74 +1,95 @@
 import React, { useContext } from "react"
-import { RecipeContext } from './App'
+import { RecipeContext } from "./App"
 import RecipeIngredientEdit from "./RecipeIngredientEdit"
 
 const RecipeEdit = ({ recipe }) => {
+  const { handleRecipeChange } = useContext(RecipeContext)
 
-    const { handleRecipeChange } = useContext(RecipeContext)
+  function handleChange(changes) {
+    handleRecipeChange(recipe.id, { ...recipe, ...changes })
+  }
 
-    function handleChange(changes) {
-        handleRecipeChange(recipe.id, { ...recipe, ...changes })
-    }
+  function handleIngredientChange(id, ingredient) {
+    const newIngredients = [...recipe.ingredients]
+    const index = newIngredients.findIndex((i) => i.id === id)
+    newIngredients[index] = ingredient
+    handleChange({ ingredients: newIngredients })
+  }
 
-    return (
-        <div className="recipe-edit">
-            <div className="recipe-edit__remove-button-container">
-                <button className="btn recipe-edit__remove-button">&times;</button>
-            </div>
-            <div className="recipe-edit__details-grid">
-                <label htmlFor="name" className="recipe-edit__label">
-                    Name
+  return (
+    <div className="recipe-edit">
+      <div className="recipe-edit__remove-button-container">
+        <button className="btn recipe-edit__remove-button">&times;</button>
+      </div>
+      <div className="recipe-edit__details-grid">
+        <label htmlFor="name" className="recipe-edit__label">
+          Name
         </label>
-                <input
-                    className="recipe-edit__input"
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={recipe.name}
-                    onInput={e => handleChange({ name: e.target.value })}
-                />
-                <label className="recipe-edit__label" htmlFor="cookTime">
-                    Cook Time
+        <input
+          className="recipe-edit__input"
+          type="text"
+          name="name"
+          id="name"
+          value={recipe.name}
+          onInput={(e) => handleChange({ name: e.target.value })}
+        />
+        <label className="recipe-edit__label" htmlFor="cookTime">
+          Cook Time
         </label>
-                <input
-                    className="recipe-edit__input"
-                    type="text"
-                    name="cookTime"
-                    id="cookTime"
-                    value={recipe.cookTime}
-                />
-                <label className="recipe-edit__label" htmlFor="servings">
-                    Servings
+        <input
+          className="recipe-edit__input"
+          type="text"
+          name="cookTime"
+          id="cookTime"
+          value={recipe.cookTime}
+          onInput={(e) => handleChange({ cookTime: e.target.value })}
+        />
+        <label className="recipe-edit__label" htmlFor="servings">
+          Servings
         </label>
-                <input
-                    className="recipe-edit__input"
-                    type="number"
-                    min="1"
-                    name="servings"
-                    id="servings"
-                    value={recipe.servings}
-                />
-                <label className="recipe-edit__label" htmlFor="instructions">
-                    Instructions
+        <input
+          className="recipe-edit__input"
+          type="number"
+          min="1"
+          name="servings"
+          id="servings"
+          value={recipe.servings}
+          onInput={(e) =>
+            handleChange({ servings: parseInt(e.target.value) || "" })
+          }
+        />
+        <label className="recipe-edit__label" htmlFor="instructions">
+          Instructions
         </label>
-                <textarea className="recipe-edit__input" name="instructions" id="instructions">{recipe.instructions}</textarea>
-            </div>
-            <br />
-            <label className="recipe-edit__label">Ingredients</label>
-            <div className="recipe-edit__ingredient-grid">
-                <div>Na
-                    me</div>
-                <div>Amount</div>
-                <div></div>
-                {recipe.ingredients.map(ingredient => {
-                    return <RecipeIngredientEdit key={ingredient.id} ingredient={ingredient} />
-                })}
-            </div>
-            <div className="recipe-edit__add-ingredient-btn-container">
-                <button className="btn btn--primary">Add Ingredient</button>
-            </div>
-        </div>
-    )
+        <textarea
+          className="recipe-edit__input"
+          name="instructions"
+          id="instructions"
+          value={recipe.instructions}
+          onInput={(e) => handleChange({ instructions: e.target.value })}
+        />
+      </div>
+      <br />
+      <label className="recipe-edit__label">Ingredients</label>
+      <div className="recipe-edit__ingredient-grid">
+        <div>Name</div>
+        <div>Amount</div>
+        <div></div>
+        {recipe.ingredients.map((ingredient) => {
+          return (
+            <RecipeIngredientEdit
+              key={ingredient.id}
+              ingredient={ingredient}
+              handleIngredientChange={handleIngredientChange}
+            />
+          )
+        })}
+      </div>
+      <div className="recipe-edit__add-ingredient-btn-container">
+        <button className="btn btn--primary">Add Ingredient</button>
+      </div>
+    </div>
+  )
 }
 
 export default RecipeEdit
--- src/components/RecipeIngredientEdit.js
+++ src/components/RecipeIngredientEdit.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react"
 
-const RecipeIngredientEdit = ({ ingredient }) => {
-    return (
-        <>
-            <input value={ingredient.name} className="recipe-edit__input" type="text" />
-            <input value={ingredient.amount} className="recipe-edit__input" type="text" />
-            <button className="btn btn--danger"> &times;</button>
-        </>
-    )
+const RecipeIngredientEdit = ({ ingredient, handleIngredientChange }) => {
+  function handleChange(changes) {
+    handleIngredientChange(ingredient.id, { ...ingredient, ...changes })
+  }
+
+  return (
+    <>
+      <input
+        value={ingredient.name}
+        onInput={(e) => handleChange({ name: e.target.value })}
+        className="recipe-edit__input"
+        type="text"
+      />
+      <input
+        value={ingredient.amount}
+        className="recipe-edit__input"
+        type="text"
+        onInput={(e) => handleChange({ amount: e.target.value })}
+      />
+      <button className="btn btn--danger"> &times;</button>
+    </>
+  )
 }
 
 export default RecipeIngredientEdit
